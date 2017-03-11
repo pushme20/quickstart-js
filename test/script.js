f430e6ae2783a3168b7e96ad9771401d688352f1
Anwana Ntofon <aontofon@cruess207-20.ucdavis.edu>
2017-03-10 17:10:02 -0800

    Removed unnecessary html closing tag

--- test/script.js
+++ test/script.js
@@ -4,6 +4,5 @@ var signInButton = document.getElementById('sign-in-button');
     signInButton.addEventListener('click', function(){
       var provider = new firebase.auth.GoogleAuthProvider();
         firebase.auth().signInWithPopup(provider);
-
     });
 });
