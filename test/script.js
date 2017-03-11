2c9ed7281b6b429602c503c071096410563e1b22
Anwana Ntofon <aontofon@cruess207-20.ucdavis.edu>
2017-03-10 17:32:51 -0800

    Completed sign in and sign out features

--- test/script.js
+++ test/script.js
@@ -4,5 +4,34 @@ window.addEventListener('load', function(){
   signInButton.addEventListener('click', function(){
     var provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithPopup(provider);
+    console.log("signInWithPopup");
   });
+
+  var signOutButton = document.getElementById('signOutButton');
+  signOutButton.addEventListener('click', function(){
+    firebase.auth().signOut();
+    console.log("signOut");
+
 });
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if(user){
+    // User is signed in.
+    var displayName = user.displayName;
+    var email = user.email;
+    var emailVerified = user.emailVerified;
+    var photoURL = user.photoURL;
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    var providerData = user.providerData;
+    console.log(user.displayName);
+    // ...
+  }
+  else {
+    // User signed out
+    /// ...
+    console.log("User logged out");
+  }
+
+
+})
