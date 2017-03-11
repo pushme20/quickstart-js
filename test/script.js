6945831cfcb501d742aab1290a0c7e8f29c82cfa
Anwana Ntofon <aontofon@cruess207-20.ucdavis.edu>
2017-03-10 17:15:11 -0800

    Changed to single quotes, removed space

--- test/script.js
+++ test/script.js
@@ -1,8 +1,8 @@
 window.addEventListener('load', function(){
 
 var signInButton = document.getElementById('sign-in-button');
-    signInButton.addEventListener('click', function(){
-      var provider = new firebase.auth.GoogleAuthProvider();
-        firebase.auth().signInWithPopup(provider);
-    });
+signInButton.addEventListener('click', function(){
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider);
+});
 });
