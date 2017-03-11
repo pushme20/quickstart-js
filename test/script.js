b5a27c828b255f783985e57b54f3edcd76e57f00
Anwana Ntofon <aontofon@cruess207-20.ucdavis.edu>
2017-03-10 17:46:33 -0800

    Added right paren

--- test/script.js
+++ test/script.js
@@ -11,23 +11,22 @@ window.addEventListener('load', function(){
   signOutButton.addEventListener('click', function(){
     firebase.auth().signOut();
     console.log("signOut");
+  });
 
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+      console.log(user.displayName);
+    } else {
+      // User is signed out.
+      console.log("User logged out");
+    }
+  });
 });
-
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    // User is signed in.
-    var displayName = user.displayName;
-    var email = user.email;
-    var emailVerified = user.emailVerified;
-    var photoURL = user.photoURL;
-    var isAnonymous = user.isAnonymous;
-    var uid = user.uid;
-    var providerData = user.providerData;
-    console.log(user.displayName);
-  } else {
-    // User is signed out.
-    console.log("User logged out");
-  }
-});
-}
